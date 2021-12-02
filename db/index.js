629db745f45e12702e568604c68648b9866760b8
Brian Walker <bw01496@gmail.com>
2021-12-01 21:12:53 -0500

    Server part 2

--- db/index.js
+++ db/index.js
@@ -348,6 +348,25 @@ async function getAllTags() {
   }
 }
 
+async function getUserByUsername(username) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+      SELECT *
+      FROM users
+      WHERE username=$1;
+    `,
+      [username]
+    );
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
+
 // and export them
 module.exports = {
   client,
@@ -364,4 +383,5 @@ module.exports = {
   addTagsToPost,
   getPostsByTagName,
   getAllTags,
+  getUserByUsername,
 };
